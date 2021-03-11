6eeff0584e09bb126eb49d88f1f82056d4bc57ed
azteckind <73482202+azteckind@users.noreply.github.com>
2021-03-11 11:08:21 -0800

    CONUS Markers
    Displaying CONUS markers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+//import mapboxgl from "mapbox-gl";
+import React, {useEffect, useState} from "react";
+import MapGL, {Marker, Popup} from "react-map-gl";
+//import * as conusData from "./data/CONUS=V2-1.geojson";
+//import conusData from "./data/CONUS_v2-1.geojson";
+import conusData from "./data/CONUS_v2-1.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function App() {
+  //Centering the viewport to the middle of the USA
+  const [viewport, setViewport] = useState ({
+    latitude: 44.58,
+    longitude: -103.46,
+    zoom: 10,
+    width: "100vw",
+    height: "100vh"
+  });
+
+  //Creating a new state to keep track of which base you've clicked on
+  const [selectedBase, setSelectedBase] = useState(null);
+
+  //Setting up an Escape option to close out the pop-up
+  useEffect(() => {
+    const listener = e => {
+      if(e.key === "Escape") {
+        setSelectedBase(null);
+      }
+    };
+    window.addEventListener("keydown", listener)
+
+    return() => {
+      window.addEventListener("keydown", listener);
+    }
+  }, []);
+  console.log(conusData)
+  return (<div>
+    <MapGL {...viewport}
+    mapboxApiAccessToken="pk.eyJ1IjoiYXp0ZWNraW5kIiwiYSI6ImNrbTB2NDA2bTAxeDQzMW12bHM5Zng0ZDkifQ.M2ow_v8f3muxTlnYeWWFgA"
+    mapStyle="mapbox://styles/azteckind/ckm2dl6sy0w8v17qyh3id9j8d"
+    onViewportChange={setViewport}
+    >
+
+      {conusData.features.map(conus => (
+        <Marker key={conus.properties.Name}
+        latitude={conus.geometry.coordinates[1]}
+        longitude={conus.geometry.coordinates[0]}>
+          <button className="marker-btn"
+          onClick={e => {
+            e.preventDefault();
+            setSelectedBase(null)
+          }}>
+            <img src="/marker-editor.svg" alt="Marker"/>
+          </button>
+        </Marker>
+      ))}
+
+      {selectedBase ? (
+        <Popup latitude={selectedBase.geometry.coordinates[1]}
+        longitude={selectedBase.geometry.coordinates[0]}
+        onClose={() => {
+          setSelectedBase(null);
+        }}>
+          <div>
+            <h2>
+              {selectedBase.properties.Name}
+            </h2>
+            <p>
+              {selectedBase.properties.Location}
+            </p>
+          </div>
+        </Popup>
+      ) : null}
+
+    </MapGL>
+  </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import mapboxgl from 'react-map-gl/src/utils/mapboxgl';
+
+mapboxgl.accessToken='pk.eyJ1IjoiYXp0ZWNraW5kIiwiYSI6ImNrbTB2NDA2bTAxeDQzMW12bHM5Zng0ZDkifQ.M2ow_v8f3muxTlnYeWWFgA';
+
+const Map = () => {
+  const mapContainer = useRef();
+  const [longitude, setLongitude] = useState(-103.46);
+  const [latitude, setLatitude] = useState(44.58);
+  const [zoom, setZoom] = useState(10);
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/azteckind/ckm2dl6sy0w8v17qyh3id9j8d",
+      center: [longitude, latitude],
+      zoom: zoom
+    });
+
+    map.on("move", () => {
+      setLatitude(map.getCenter().latitude.toFixed(4));
+      setLongitude(map.getCenter().longitude.toFixed(4));
+      setZoom(map.getZoom().toFixed(2));
+    });
+
+    return () => map.remove();
+  }, []);
+
+  return(
+    <div>
+      <div className="sidebar">
+        Longitude: {longitude} | Latitude: {latitude} | Zoom: {zoom}
+      </div>
+      <div className="map-container" ref={mapContainer}/>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +47,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
