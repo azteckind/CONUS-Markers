12c9db6d0261d3463c459efd00b224e3bd85486b
azteckind <73482202+azteckind@users.noreply.github.com>
2021-03-11 14:39:51 -0800

    Deleted extra spacing

--- src/App.js
+++ src/App.js
@@ -71,8 +71,7 @@ export default function App() {
           </div>
         </Popup>
       )}
-
-    </ReactMapGL>
+     </ReactMapGL>
   </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {useRef, useEffect, useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -7,38 +7,38 @@ import mapboxgl from 'react-map-gl/src/utils/mapboxgl';
 
 mapboxgl.accessToken='pk.eyJ1IjoiYXp0ZWNraW5kIiwiYSI6ImNrbTB2NDA2bTAxeDQzMW12bHM5Zng0ZDkifQ.M2ow_v8f3muxTlnYeWWFgA';
 
-const Map = () => {
-  const mapContainer = useRef();
-  const [longitude, setLongitude] = useState(-103.46);
-  const [latitude, setLatitude] = useState(44.58);
-  const [zoom, setZoom] = useState(10);
+// const Map = () => {
+//   const mapContainer = useRef();
+//   const [longitude, setLongitude] = useState(-103.46);
+//   const [latitude, setLatitude] = useState(44.58);
+//   const [zoom, setZoom] = useState(10);
 
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/azteckind/ckm2dl6sy0w8v17qyh3id9j8d",
-      center: [-103.46,44.58],
-      zoom: 10
-    });
+//   useEffect(() => {
+//     const map = new mapboxgl.Map({
+//       container: mapContainer.current,
+//       style: "mapbox://styles/azteckind/ckm2dl6sy0w8v17qyh3id9j8d",
+//       center: [-103.46,44.58],
+//       zoom: 10
+//     });
 
-    map.on("move", () => {
-      setLatitude(map.getCenter().latitude);
-      setLongitude(map.getCenter().longitude);
-      setZoom(map.getZoom());
-    });
+//     map.on("move", () => {
+//       setLatitude(map.getCenter().latitude);
+//       setLongitude(map.getCenter().longitude);
+//       setZoom(map.getZoom());
+//     });
 
-    return () => map.remove();
-  }, []);
+//     return () => map.remove();
+//   }, []);
 
-  return(
-    <div>
-      <div className="sidebar">
-        Longitude: {longitude} | Latitude: {latitude} | Zoom: {zoom}
-      </div>
-      <div className="map-container" ref={mapContainer}/>
-    </div>
-  )
-}
+//   return(
+//     <div>
+//       <div className="sidebar">
+//         Longitude: {longitude} | Latitude: {latitude} | Zoom: {zoom}
+//       </div>
+//       <div className="map-container" ref={mapContainer}/>
+//     </div>
+//   )
+// }
 
 ReactDOM.render(
   <React.StrictMode>
